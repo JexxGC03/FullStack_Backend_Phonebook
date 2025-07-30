42d5c628e4afd81b075bdc1339c3ac82ecd59071
Jesus G <jgaitan345@gmail.com>
2025-07-30 08:05:56 -0500

    feat: ui build and deploy with exercises 3.9-3.11

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan');
+const cors = require('cors')
 
-app.use(express.json())
+app.use(cors())
 
 let persons = [
     { 
@@ -27,13 +28,20 @@ let persons = [
     }
 ]
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+} 
 
-app.use(morgan('dev'));
+app.use(express.json())
+//app.use(morgan('dev'));
+app.use(express.static('dist'))
+app.use(requestLogger)
 
+/*
 morgan.token('post-data', (req) => {
   if (req.method === 'POST') {
     return JSON.stringify(req.body);
@@ -42,22 +50,23 @@ morgan.token('post-data', (req) => {
 });
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'));
- 
+*/
+
 app.get('/', (request, response) => {
   response.send(`<h1>Backend Phonebook</h1>` )
 })
 
 app.get('/info', (request, response) => {
-    const total_persons = persons.length
-    console.log(total_persons)
-    const date = new Date().toString()
-    response.send(
-        `<p>Phonebook has info for ${total_persons} people</p>
-        <p>${date} </p>`)
-})
-
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  const total_persons = persons.length
+  console.log(total_persons)
+  const date = new Date().toString()
+  response.send(
+    `<p>Phonebook has info for ${total_persons} people</p>
+    <p>${date} </p>`)
+  })
+  
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -71,14 +80,12 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
-})
 
 const generateId = () => {
-  return Math.floor(Math.random() * (99999 - 1 + 1) + 1);
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
 }
 
 app.post('/api/persons', (request, response) => {
@@ -90,21 +97,31 @@ app.post('/api/persons', (request, response) => {
       error: 'content missing' 
     })
   }
-
+  
   if(existingPerson){
     return response.status(400).json({
-        error: `person ${body.name} already exists`
+      error: `person ${body.name} already exists`
     })
   }
-
+  
   const person = {
     name: body.name,
     number: body.number,
     id: generateId(),
   }
-
+  
   persons = persons.concat(person)
-
+  
   response.json(person)
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
